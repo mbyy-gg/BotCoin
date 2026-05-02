2d34eb893d6bfa539898e7684c9150fe37077ec6
GitHub Action <action@github.com>
2026-05-02 10:47:55 +0000

    Auto-extract zip files [skip actions]

---
+++ Bot Coin By Alby/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    botToken: '8696627610:AAHgiGjpapfc4_oqYlrQTk3NrHrn5H-xWsc', // Isi token dari @BotFather
+    ownerId: 5676072619,
+    channels: ['@botcoinalby', '@seputarinfobotcoinalby'], // Username channel wajib join
+    group: '@roompublicalbytzy', // Username grup wajib join
+    notifChannel: '@penukarancoinalby', // Channel laporan penukaran
+    startImage: 'https://files.catbox.moe/rbqlhl.jpeg' // Gambar menu utama
+};
